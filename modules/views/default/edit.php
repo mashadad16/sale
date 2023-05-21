617901d5282371ba13fd48aaadf6d0f9e2f667d2
Sparky <mmd@9-33.com>
2023-05-21 19:10:26 +0300

    Реализованы методы для создания, редактирования, удаления заказа. Реализован фильтр по имени заказа.

--- modules/views/default/edit.php
+++ modules/views/default/edit.php
@@ -8,7 +8,10 @@ use yii\helpers\Html;
             <div class="col-lg-5">
                 <?php $form = ActiveForm::begin(['id' => 'order-form']); ?>
                     <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
-                    <?= $form->field($model, 'status_id')->textInput(['autofocus' => true]) ?>
+                    <?= $form->field($model, 'status_id')->dropDownList([
+                        '1' => 'Выполнен',
+                        '2'=>'В работе'
+                    ]); ?>
                     <?= $form->field($model, 'create_at')->input('date'); ?>
                     <div class="form-group">
                         <?= Html::submitButton('Сохранить', ['class' => 'btn btn-warning', 'name' => 'contact-button']) ?>
