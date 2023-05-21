ab44463b19af1cd65e69e37b8fb8fca20b5fb58d
Sparky <mmd@9-33.com>
2023-05-21 16:57:28 +0300

    Подключена библиотека Font Awesome, для кастомизации кнопок действий в dataprovider.

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,6 +21,8 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'https://use.fontawesome.com/releases/v5.5.0/css/all.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
     ];
     public $js = [
     ];
